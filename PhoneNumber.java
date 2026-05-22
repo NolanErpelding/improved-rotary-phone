366b8b9852ef57519f2e815f156519c8cca1355a
nolan <nolan.erpelding@gmail.com>
2026-05-22 12:53:14 -0500

    added phone number class

---
+++ PhoneNumber.java
@@ -0,0 +1,18 @@
+public class PhoneNumber {
+    private int number;
+
+    public PhoneNumber() {
+    }
+    
+    public PhoneNumber(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+}
