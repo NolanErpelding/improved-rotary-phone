30bf78d9f337d841bdc7295083c7361e341a7515
andrewsiagian123 <andrew.t.siagian@gmail.com>
2026-05-22 13:07:39 -0500

    Added input and scanner in Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,12 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.out.println("Lets go andrew.");
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a phone number: ");
+        int number = input.nextInt();
     }
 }
