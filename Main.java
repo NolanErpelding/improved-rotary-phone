f83ead843916451e841c433e3f01fa41b73b2717
nolan <nolan.erpelding@gmail.com>
2026-05-25 19:40:51 -0500

    added sentimentvlaues.txt

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.println("Enter a phone number (we recommend excluding the country code): ");
+        System.out.print("Enter a phone number: ");
         String number = input.nextLine();
         PhoneNumber phone = new PhoneNumber(number);
         input.close();
