99c98d815063ac04c9a0adf6b3baeb1b068426fc
andrewsiagian123 <andrew.t.siagian@gmail.com>
2026-05-26 13:31:04 -0500

    Scanner for Words

--- Words.java
+++ Words.java
@@ -1,5 +1,33 @@
 import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
 
-public class Words {
+public class Words { 
+    public static void main(String[] args) {
+        File sentimentValues = new File("SentimentValues.txt");
+
+        // The try-with-resources block automatically handles closing the reader
+        try (Scanner reader = new Scanner(sentimentValues)) {
+            
+            while (reader.hasNextLine()) {
+                String data = reader.nextLine();
+                // You can now use the 'data' variable here
+                System.out.println(data); 
+            }
+            
+        } catch (FileNotFoundException e) {
+            System.out.println("Error: The file 'SentimentValues.txt' could not be found.");
+            e.printStackTrace();
+        }
+    }
     
+    /* File sentimentValues = new File("SentimentValues.txt");
+     Scanner reader = new Scanner(sentimentValues);
+
+     while(reader.hasNextLine()){
+        String data = reader.nextLine();
+     }
+
+     reader.close(); */
+
 }
