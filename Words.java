6b85819b01a23de0c4fe974d9486c43c0ae5b92e
nolan <nolan.erpelding@gmail.com>
2026-05-27 19:18:11 -0500

    fixed static

--- Words.java
+++ Words.java
@@ -5,34 +5,37 @@ import java.util.ArrayList;
 
 public class Words {
     
-    private ArrayList<String> words;
-    private ArrayList<Double> sentiment;
+    private static ArrayList<String> words;
+    private static ArrayList<Double> sentiment;    
+    private static File sentimentValues = new File("SentimentValues.txt");
 
-    public void Words() {
-        File sentimentValues = new File("SentimentValues.txt");
-
+    // This is a static block its kinda like an initializer for static stuff
+    //I wrote this not AI alright VEDANT!
+    static {
+        words = new ArrayList<>();
+        sentiment = new ArrayList<>();
         try (Scanner reader = new Scanner(sentimentValues)) {
-            
+
             while (reader.hasNextLine()) {
                 String data = reader.nextLine();
                 words.add(data.substring(0, data.indexOf(",")));
                 sentiment.add(Double.parseDouble(data.substring(data.indexOf(",") + 1)));
             }
-            
+
         } catch (FileNotFoundException e) {
             System.out.println("Error: The file 'SentimentValues.txt' could not be found.");
             e.printStackTrace();
         }
     }
     
-    public ArrayList<String> getWords() {
+    public static ArrayList<String> getWords() {
         return words;
     }
 
-    public ArrayList<Double> getSentiment() {
+    public static ArrayList<Double> getSentiment() {
         return sentiment;
     }
-    public boolean isWord(String word) {
+    public static boolean isWord(String word) {
         return words.contains(word);
     }
 }
