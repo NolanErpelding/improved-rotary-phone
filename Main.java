028c5b16dec5b937c76115e87440197c112fc934
andrewsiagian123 <andrew.t.siagian@gmail.com>
2026-05-28 12:56:56 -0500

    added if statement in main

--- Main.java
+++ Main.java
@@ -6,11 +6,18 @@ public class Main {
         Scanner input = new Scanner(System.in);
         System.out.println("");
         System.out.print("Enter a phone number: ");
-        String number = input.nextLine();
-        //System.out.print(Words.getWords());
-        //System.out.println(Words.getWords().size());
+        if (input.hasNextLine()) {
+            String number = input.nextLine();
+            System.out.println("\n\n");
+            PhoneNumber phone = new PhoneNumber(number);
+        } else {
+            System.out.println("\nError: No input detected from the console environment!");
+        }
+        /* String number = input.nextLine();
+        System.out.print(Words.getWords());
+        System.out.println(Words.getWords().size());
         System.out.println("\n\n");
-        PhoneNumber phone = new PhoneNumber(number);
+        PhoneNumber phone = new PhoneNumber(number); */
         input.close();
     }
 }
